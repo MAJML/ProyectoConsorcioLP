5503ec9df325d68937d9ebbc3e75924b846ed7b8
Marco Antonio <marco.antonio.9956@gmail.com>
2023-07-19 01:05:05 -0500

    avanze de toda la ventana de servicios

--- view/servicios.php
+++ view/servicios.php
@@ -1,11 +1,13 @@
 <section class="seccion_servicios">
     <h5 class="titulo_nuestros_servicios">Nuestros Servicios</h5>
     <div class="caja_general_servicios">
-        <div class="item_servicios">Marketing y Promociones, Campañas & BTL</div>
-        <div class="item_servicios">Tercerización y Outsourcing de procesos</div>
-        <div class="item_servicios">Limpieza General, Facility Management y Servicios Complementarios</div>
-        <div class="item_servicios">Personal temporal e Intermediación</div>
-        <div class="item_servicios">Consultoría y Gestión Humana</div>
-        <div class="item_servicios">Servicios Complementarios y Especializados</div>
+        <div class="item_servicios border_primero">Marketing y Promociones, Campañas & BTL</div>
+        <div class="item_servicios segundo_item">Tercerización y Outsourcing de procesos</div>
+        <div class="item_servicios tercero_item">Limpieza General, Facility Management y Servicios Complementarios</div>
+        <div class="item_servicios cuarto_item">Personal temporal e Intermediación</div>
+        <div class="item_servicios quinto_item">Consultoría y Gestión Humana</div>
+        <div class="item_servicios border_ultimo">Servicios Complementarios y Especializados</div>
     </div>
 </section>
+
+
